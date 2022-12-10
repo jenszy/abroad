dc73a2aac95d449e45b39d71586acb95dcacab36
jenszy <120130745+jenszy@users.noreply.github.com>
2022-12-10 15:46:37 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,19 @@
+function resumeIMGHide(){
+    
+	// grabs the input from the user
+    
+    document.getElementById("resume-img").style.visibility = "hidden";
+
+
+
+}
+
+function resumeIMGView(){
+    
+	// grabs the input from the user
+    
+    document.getElementById("resume-img").style.visibility = "visible";
+
+
+
+}
